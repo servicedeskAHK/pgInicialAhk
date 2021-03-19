9c0200c307f8c5c4df4591ac52fc1f9445842124
Felipe Alexandre <felipealexandre98@gmail.com>
2021-03-19 17:31:49 -0300

    InicialAHKSalesforce

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import '../styles/Home.module.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,87 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <> 
+      <header>
+        <div className="navbar navbar-dark bg-dark shadow-sm" >
+          <div className="container" >
+            <a href="#" className="navbar-brand d-flex align-items-center">
+            <Image src="/camara.png" width="350px" height="72px" />
+            </a>
+          </div>
         </div>
-      </main>
+      </header>
+      <section className="py-3 text-center container">
+        
+      </section>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+      <div style={{margin: '0px 20px 0px 20px', height: "100%"}}>
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4">
+          
+          <div className="col" style={{margin: '0px 0px 20px 0px'}}>
+            <div className={styles.containerOverlay}>
+              <Image src="/sp.png" className={styles.image}  alt="AHK" width="800px" height="1000px"/> 
+              <div className={styles.overlay}>                
+              </div>
+              <h5 className={styles.text}>São Paulo</h5>
+            </div>
+          </div>
+
+          <div className="col" style={{margin: '0px 0px 20px 0px'}}>
+            <div className={styles.containerOverlay}>
+              <Image src="/rj.jpg" className={styles.image} alt="AHK" width="800px" height="1000px"/>
+              <div className={styles.overlay}>                
+              </div>
+              <h5 className={styles.text}>Rio de Janeiro</h5>
+            </div>
+          </div>
+
+          <div className="col" style={{margin: '0px 0px 20px 0px'}}>
+            <div className={styles.containerOverlay}>
+              <Image src="/poa.jpg" className={styles.image} alt="AHK" width="800px" height="1000px"/>
+              
+              <div className={styles.overlay}>                
+              </div>
+              <h5 className={styles.text}>Porto Alegre</h5>
+            </div>
+          </div>
+
+          <div className="col" style={{margin: '0px 0px 20px 0px'}}>
+            <div className={styles.containerOverlay}>
+              <Image src="/cur.jpg" className={styles.image} alt="AHK" width="800px" height="1000px"/>
+              <div className={styles.overlay}>                
+              </div>
+              <h5 className={styles.text}>Paraná</h5>
+            </div>
+          </div>
+
+          
+
+
+        </div>
+      </div>
+      
+      <footer className="text-muted py-5">
+      <div className="d-flex justify-content-end">
+            <a href="https://www.instagram.com/ahkbrasil/"><img className={styles.footer_image} src="/instagram.png" /></a>
+            <a href="https://www.linkedin.com/company/ahkbrasil/"><img className={styles.footer_image} src="/linkedin.png" /></a>            
+            <a href="https://www.youtube.com/channel/UCDdSnLH1FuLCiXFEjBOJy7A"><img className={styles.footer_image} src="/youtube.png" /></a>            
+            <a href="https://t.me/joinchat/Q1WYhRiwei7aNgJhWlM3UA"><img className={styles.footer_image} src="/telegram.png" /></a>
+          </div>
+      <hr></hr>
+        <div className="container text-center">
+        
+          <a href="http://www.bmwi.de/"><img src="/banner_1.png" className={styles.footer_image} alt="AHK" width="128px" height="120px" /></a>
+          <a href="http://www.bmwi.de/"><img src="/banner_2..png"  className={styles.footer_image} alt="AHK" width="100px" height="49px" /></a>
+          <a href="http://www.bmwi.de/"><img src="/banner_3..png" className={styles.footer_image} alt="AHK" width="100px" height="49px" /></a>
+          <a href="http://www.bmwi.de/"><img src="/banner_4.png" className={styles.footer_image} alt="AHK" width="136px" height="20px" /></a>
+          
+        </div>
       </footer>
-    </div>
+    </>
   )
 }
